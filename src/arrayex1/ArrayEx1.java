eff0e1869fb424c009ab9e3bece193de4e181ece
destinybravos <destinybravos@gmail.com>
2022-04-21 17:44:54 +0100

    Added a space

--- src/arrayex1/ArrayEx1.java
+++ src/arrayex1/ArrayEx1.java
@@ -8,7 +8,7 @@ public class ArrayEx1 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String []sname = new String[10];        
-        int []age = new int[10];
+        int []age = new int[10]; 
         int N;
         
         for (N = 0; N <= 2; N++) {
