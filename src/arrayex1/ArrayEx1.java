01b3a8489107e9b3238865ad3e7f65e5c04a8f3d
destinybravos <destinybravos@gmail.com>
2022-04-21 17:42:39 +0100

    I deleted a Variable

--- src/arrayex1/ArrayEx1.java
+++ src/arrayex1/ArrayEx1.java
@@ -10,7 +10,6 @@ public class ArrayEx1 {
         String []sname = new String[10];        
         int []age = new int[10];
         int N;
-        int A = 23;
         
         for (N = 0; N <= 2; N++) {
             System.out.print("Enter the name of student " + (N+1));
